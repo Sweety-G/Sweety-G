9ba69cd7a7076d0ef20803f0d96544ca696a9ea5
Sweety-G <isweetygpt@gmail.com>
2026-08-23 19:51:44 +0530

    Add more profile stats

--- scripts/test.py
+++ scripts/test.py
@@ -1,35 +1,59 @@
 import requests
 
-# Get GitHub data
-url = "https://api.github.com/users/Sweety-G"
-response = requests.get(url)
-data = response.json()
+username = "Sweety-G"
 
-username = data["login"]
-repos = data["public_repos"]
-followers = data["followers"]
+# Get profile information
+profile_url = f"https://api.github.com/users/{username}"
+profile_response = requests.get(profile_url)
+profile_data = profile_response.json()
 
-# Create SVG using the GitHub data
+repos = profile_data["public_repos"]
+followers = profile_data["followers"]
+following = profile_data["following"]
+
+# Get repositories to calculate total stars
+repos_url = f"https://api.github.com/users/{username}/repos?per_page=100"
+repos_response = requests.get(repos_url)
+repositories = repos_response.json()
+
+total_stars = 0
+
+for repo in repositories:
+    total_stars += repo["stargazers_count"]
+
+# Create SVG
 svg = f"""
-<svg xmlns="http://www.w3.org/2000/svg" width="500" height="150">
-    <rect width="500" height="150" fill="white"/>
+<svg xmlns="http://www.w3.org/2000/svg" width="600" height="260">
+    <rect width="600" height="260" rx="15" fill="white"/>
 
-    <text x="30" y="45" font-size="28">
+    <text x="40" y="55" font-size="32" font-family="Arial">
         {username}
     </text>
 
-    <text x="30" y="85" font-size="20">
+    <text x="40" y="105" font-size="22" font-family="Arial">
         Repositories: {repos}
     </text>
 
-    <text x="30" y="120" font-size="20">
+    <text x="40" y="145" font-size="22" font-family="Arial">
         Followers: {followers}
+    </text>
+
+    <text x="40" y="185" font-size="22" font-family="Arial">
+        Following: {following}
+    </text>
+
+    <text x="40" y="225" font-size="22" font-family="Arial">
+        Stars: {total_stars}
     </text>
 </svg>
 """
 
-# Save SVG
+# Save the SVG
 with open("profile.svg", "w") as file:
     file.write(svg)
 
-print("Profile SVG created!")
+print("Profile stats generated!")
+print(f"Repositories: {repos}")
+print(f"Followers: {followers}")
+print(f"Following: {following}")
+print(f"Stars: {total_stars}")
